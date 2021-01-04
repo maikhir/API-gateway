ee1e1cb58f9b0150527e932c13a8e92f522155ea
Maik Hirthe <Maik.Hirthe@rohde-schwarz.com>
2021-01-04 10:35:12 +0100

    first import

---
+++ src/test/java/de/hirthe/ffw/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hirthe.ffw.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
